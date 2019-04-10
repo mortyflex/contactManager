edcd10326cc5b9e7cd05c288ffa09c0025e18efb
mortyflex <mortyflex@gmail.com>
2019-04-11 00:17:07 +0700

    Updates

---
+++ precache-manifest.ac7abcf8f8b2c8cf0a4779bf92f549c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a95665d41ca1e795fe",
+    "url": "/contactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "22a95665d41ca1e795fe",
+    "url": "/contactManager/static/js/main.baf1fd41.chunk.js"
+  },
+  {
+    "revision": "49225faa5712f33c0f72",
+    "url": "/contactManager/static/js/runtime~main.63e620a2.js"
+  },
+  {
+    "revision": "30d11dbc97e2b9146487",
+    "url": "/contactManager/static/css/2.c34ae987.chunk.css"
+  },
+  {
+    "revision": "30d11dbc97e2b9146487",
+    "url": "/contactManager/static/js/2.946eab7a.chunk.js"
+  },
+  {
+    "revision": "8cc3836d1a943187dd5491ae237764f5",
+    "url": "/contactManager/index.html"
+  }
+];
