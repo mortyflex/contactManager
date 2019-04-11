d637bcc40a719899f6524ad736e0c2e05b151a60
mortyflex <mortyflex@gmail.com>
2019-04-12 00:23:52 +0700

    fix deploy error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
 import EditContact from "./components/contacts/EditContact";
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename="/">
           <div className="App">
             <Header branding="Contact Manager" />
             <div className="container">
